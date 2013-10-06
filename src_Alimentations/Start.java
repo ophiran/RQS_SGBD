2695f11139ff1a56d129c713cea35a53cf6e731c
ophiran <ophiran@hotmail.com>
2013-10-06 17:15:44 +0200

    Refactor

--- src_Alimentations/Start.java
+++ src_Alimentations/Start.java
@@ -1,12 +1,12 @@
+import dbAccess.DBAccess;
 import dbAccess.CouchDBAccess;
-import dbAccess.CouchDBAccessImpl;
 
 
 public class Start {
 
 	public static void main(String[] args) {
 		//utilisation de l'argument -console pour passer en mode console?
-		CouchDBAccess test = new CouchDBAccessImpl();
+		DBAccess test = new CouchDBAccess();
 		test.connect("127.0.0.1", "5984");
 		
 		
--- src_Alimentations/dbAccess/CouchDBAccess.java
+++ src_Alimentations/dbAccess/CouchDBAccess.java
@@ -1,15 +1,62 @@
 package dbAccess;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.ektorp.CouchDbConnector;
+import org.ektorp.CouchDbInstance;
+import org.ektorp.ViewQuery;
 import org.ektorp.ViewResult;
+import org.ektorp.http.HttpClient;
+import org.ektorp.http.StdHttpClient;
+import org.ektorp.impl.StdCouchDbConnector;
+import org.ektorp.impl.StdCouchDbInstance;
 
+public class CouchDBAccess implements DBAccess {
 
-public interface CouchDBAccess {
-
-	void connect(String ip,String port);
-	void bindDb(String dbName);
-	void close();
+	Logger log;
+	HttpClient httpClient;
+	CouchDbInstance dbInstance;
+	CouchDbConnector db;
 	
-	ViewResult sendQuery(String docId,String viewName);
-	ViewResult sendQuery(String query);
+	public CouchDBAccess() {
+		log = Logger.getLogger("CouchDBAccess Logger");
+	}
 	
-	//void createIndex(String table,String column,Class MapType);
+	@Override
+	public void connect(String ip, String port) {
+		httpClient = new StdHttpClient.Builder().host(ip).port(Integer.parseInt(port)).build();
+		dbInstance = new StdCouchDbInstance(httpClient);
+		log.log(Level.INFO, "Connected to "  + ip + ":" + port);
+	}
+
+	@Override
+	public void bindDb(String dbName) {
+		if(db != null){
+			db = new StdCouchDbConnector(dbName, dbInstance);
+			log.log(Level.INFO, "Binded to db " + dbName);
+		}
+		else {
+			log.log(Level.WARNING, "Binding to a non-connected DB");
+		}
+	}
+
+	@Override
+	public void close() {
+		if(httpClient != null) {
+			httpClient.shutdown();
+		}
+	}
+
+	@Override
+	public ViewResult sendQuery(String docId, String viewName) {
+        ViewQuery query = new ViewQuery().designDocId(docId).viewName(viewName);
+		return db.queryView(query);
+	}
+
+	@Override
+	public ViewResult sendQuery(String query) {
+		return null;
+	}
+
 }
