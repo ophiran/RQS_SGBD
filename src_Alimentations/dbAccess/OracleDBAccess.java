cf09c1e3c245364a176345b828ea44b6c30f12d2
ophiran <ophiran@hotmail.com>
2013-10-25 20:31:56 +0200

    -OracleDBAccess

---
+++ src_Alimentations/dbAccess/OracleDBAccess.java
@@ -0,0 +1,53 @@
+package dbAccess;
+
+import java.sql.Array;
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Collection;
+import java.util.List;
+
+import oracle.jdbc.OracleDriver;
+import oracle.sql.ARRAY;
+import oracle.sql.ArrayDescriptor;
+
+public class OracleDBAccess {
+	
+	private Connection connection;
+	
+	public OracleDBAccess() throws SQLException {
+		DriverManager.registerDriver(new OracleDriver());
+	}
+	
+	public synchronized void startConnection(String dbPath,String login,String password) throws SQLException{
+		connection = DriverManager.getConnection("jdbc:oracle:thin:"+dbPath, login, password);
+	}
+	
+	public synchronized void stopConnection() throws SQLException{
+		if(connection!=null)
+			connection.close();
+	}
+	
+	public Connection getConnection(){
+		return connection;
+	}
+	
+	public synchronized void sendObject(String type,List<Object> object) throws SQLException{
+		sendObject(type, object.toArray());
+	}
+	
+	public synchronized void sendObject(String type,Collection<Object> object) throws SQLException{
+		sendObject(type,object.toArray());
+	}
+	
+	public synchronized void sendObject(String type,Object[] object) throws SQLException{
+		ArrayDescriptor aDesc = ArrayDescriptor.createDescriptor(type, getConnection());
+		
+		CallableStatement statement = connection.prepareCall("{call test_array(?)}");
+		statement.setArray(1, new ARRAY(aDesc, connection, object));
+		statement.execute();
+		statement.close();
+		connection.commit();
+	}
+}
